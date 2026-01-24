f9074d717a416072044ac646b832fb40b6189310
brendanpatrickfineran <bfineran727@gmail.com>
2026-01-24 14:59:21 -0500

    added content

---
+++ src/resources/content.tsx
@@ -0,0 +1,259 @@
+import { About, Blog, Gallery, Home, Newsletter, Person, Social, Work } from "@/types";
+import { Line, Row, Text } from "@once-ui-system/core";
+
+const person: Person = {
+  firstName: "Brendan",
+  lastName: "Fineran",
+  name: `Brendan Fineran`,
+  role: "Musician",
+  avatar: "/images/avatar.jpg",
+  email: "bfineran727@gmail.com",
+  location: "America/Detroit", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English"], // optional: Leave the array empty if you don't want to display languages
+};
+
+const newsletter: Newsletter = {
+  display: false,
+  title: <>Subscribe to {person.firstName}'s Newsletter</>,
+  description: <>My weekly newsletter about creativity and engineering</>,
+};
+
+const social: Social = [
+  // Links are automatically displayed.
+  // Import new icons in /once-ui/icons.ts
+  // Set essentials: true for links you want to show on the about page
+  {
+    name: "GitHub",
+    icon: "github",
+    link: "https://github.com/once-ui-system",
+    essential: false,
+  },
+  {
+    name: "Instagram",
+    icon: "instagram",
+    link: "https://www.instagram.com/once_ui/",
+    essential: true,
+  },
+  {
+    name: "Email",
+    icon: "email",
+    link: `mailto:${person.email}`,
+    essential: true,
+  },
+  {
+    name: "Phone",
+    icon: "phone",
+    link: 'tel:2488054566',
+    essential: true,
+  }
+];
+
+const home: Home = {
+  path: "/",
+  image: "/images/og/home.jpg",
+  label: "Home",
+  title: `${person.name}'s Portfolio`,
+  description: `Portfolio website showcasing my work as a ${person.role}`,
+  headline: <>Brendan Fineran</>,
+  featured: {
+    display: false,
+    title: (
+      <Row gap="12" vertical="center">
+        <strong className="ml-4">Once UI</strong>{" "}
+        <Line background="brand-alpha-strong" vert height="20" />
+        <Text marginRight="4" onBackground="brand-medium">
+          Featured work
+        </Text>
+      </Row>
+    ),
+    href: "/work/building-once-ui-a-customizable-design-system",
+  },
+  subline: (
+    <>
+      I'm Brendan Fineran, a guitar player in Detroit and Boston. I'm currently attending Berklee College of Music. I teach, gig, and write. Contact me if you're intrested in any of my services!
+    </>
+  ),
+};
+
+const about: About = {
+  path: "/about",
+  label: "About",
+  title: `About – ${person.name}`,
+  description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+  tableOfContent: {
+    display: true,
+    subItems: false,
+  },
+  avatar: {
+    display: true,
+  },
+  calendar: {
+    display: false,
+    link: "https://cal.com",
+  },
+  intro: {
+    display: true,
+    title: "Introduction",
+    description: (
+      <>
+        Brendan is a multi-instrumentalist with a focus on guitar.
+        He has been playing and gigging for the past ten years.
+        He has dedicated his life to music, with his work spanning
+        production and engineering, songwriting, performance, and teaching.
+      </>
+    ),
+  },
+  work: {
+    display: true, // set to false to hide this section
+    title: "Experience",
+    experiences: [
+      {
+        company: "Acromag",
+        timeframe: "2024 - 2025",
+        role: "Quality Control Software Engineer",
+        achievements: [
+          <>
+            I developed a camera system to document products before they were
+            shipped to protect the company and ensure accountability and
+            responsibility.
+          </>,
+          <>
+            I also coordinated with the shipping and final assembly
+            departments and implemented barcodes on our inventory.
+          </>,
+        ],
+        images: [
+          // optional: leave the array empty if you don't want to display images
+          /*
+          {
+            src: "",
+            alt: "",
+            width: 16,
+            height: 9,
+          },
+          */
+        ],
+      },
+      {
+        company: "School of Rock All Stars",
+        timeframe: "2024",
+        role: "Guitar",
+        achievements: [
+          <>
+            Passed all three rounds of auditions and got to play with the top 1% of students
+            at School of Rock touring the Northeastern United States.
+          </>
+        ],
+        images: [],
+      },
+    ],
+  },
+  studies: {
+    display: true, // set to false to hide this section
+    title: "Studies",
+    institutions: [
+      {
+        name: "Berklee College of Music",
+        description: <>Currently Studying Music Production and Engineering</>,
+      },
+    ],
+  },
+  technical: {
+    display: false, // set to false to hide this section
+    title: "Technical skills",
+    skills: [
+      {
+        title: "Figma",
+        description: (
+          <>Able to prototype in Figma with Once UI with unnatural speed.</>
+        ),
+        tags: [
+          {
+            name: "Figma",
+            icon: "figma",
+          },
+        ],
+        // optional: leave the array empty if you don't want to display images
+        images: [
+          {
+            src: "/images/projects/project-01/cover-02.jpg",
+            alt: "Project image",
+            width: 16,
+            height: 9,
+          },
+          {
+            src: "/images/projects/project-01/cover-03.jpg",
+            alt: "Project image",
+            width: 16,
+            height: 9,
+          },
+        ],
+      },
+      {
+        title: "Next.js",
+        description: (
+          <>Building next gen apps with Next.js + Once UI + Supabase.</>
+        ),
+        tags: [
+          {
+            name: "JavaScript",
+            icon: "javascript",
+          },
+          {
+            name: "Next.js",
+            icon: "nextjs",
+          },
+          {
+            name: "Supabase",
+            icon: "supabase",
+          },
+        ],
+        // optional: leave the array empty if you don't want to display images
+        images: [
+          {
+            src: "/images/projects/project-01/cover-04.jpg",
+            alt: "Project image",
+            width: 16,
+            height: 9,
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const blog: Blog = {
+  path: "/blog",
+  label: "Gigs",
+  title: "Writing about music and gigs...",
+  description: `Read what ${person.name} has been up to recently`,
+  // Create new blog posts by adding a new .mdx file to app/blog/posts
+  // All posts will be listed on the /blog route
+};
+
+const work: Work = {
+  path: "/work",
+  label: "Works",
+  title: `Projects – ${person.name}`,
+  description: `Design and dev projects by ${person.name}`,
+  // Create new project pages by adding a new .mdx file to app/blog/posts
+  // All projects will be listed on the /home and /work routes
+};
+
+const gallery: Gallery = {
+  path: "/gallery",
+  label: "Gallery",
+  title: `Photo gallery – ${person.name}`,
+  description: `A photo collection by ${person.name}`,
+  // Images by https://lorant.one
+  // These are placeholder images, replace with your own
+  images: [
+    {
+      src: "/images/gallery/horizontal-1.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+  ],
+};
+
+export { person, social, newsletter, home, about, blog, work, gallery };
